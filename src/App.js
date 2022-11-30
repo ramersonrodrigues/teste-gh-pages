4d5dea7a6a64be74899a5b270456163bf21c79cf
Ramerson Rodrigues <ramersonrodrigues.ti@gmail.com>
2022-11-30 09:20:21 -0300

    save

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 function App() {
   return (
     <div style={{ width: "100%", height: "100vh", display: "flex", justifyContent: "center", alignItems: "center" }}>
-      <label>GH Pages {process.env.REACT_APP_TECNOLOGIA} </label>
+      <label>GH Pages {process.env.REACT_APP_TECNOLOGIA}. </label>
     </div>
   );
 }
